798a5428195b3a475cef189729a30f7eedd87ccf
Chadwick Flores <chadwickflores@gmail.com>
2014-09-15 19:45:30 -0500

    udpate books controller for api module

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,32 +1,35 @@
-class BooksController < ApplicationController
-	def index
-    books = Book.all
+module Api
+  class BooksController < ApplicationController
+    def index
+      books = Book.available
 
-    if rating = params[:rating]
-    	books = books.where(rating: rating)
+      if rating = params[:rating]
+      	books = books.where(rating: rating)
+      end
+      
+      render json: books, status: 200
     end
-    
-    render json: books, status: 200
-  end
 
-  def create
-  	book = Book.new(book_params)
-  	if book.save
-  		render json: book, status: 201, location: book
-  	else
-  		render json: book.errors, status: 422
-  	end
-  end
+    def create
+    	book = Book.new(book_params)
+    	if book.save
+    		render json: book, status: 201, location: book
+    	else
+    		render json: book.errors, status: 422
+    	end
+    end
 
-  def destroy
-  	book = Book.find(params[:id])
-  	book.destroy!
-  	render nothing: true, status: 204
-  end
+    def destroy
+    	book = Book.find(params[:id])
+    	book.archive
+    	head 204
+    end
 
-  def book_params
-  	params.require(:book).permit(:title, :rating, 
-                                :author, :genre_id, 
-                                :review, :amazon_id)
+    private
+    def book_params
+    	params.require(:book).permit(:title, :rating, 
+                                  :author, :genre_id, 
+                                  :review, :amazon_id)
+    end
   end
 end
